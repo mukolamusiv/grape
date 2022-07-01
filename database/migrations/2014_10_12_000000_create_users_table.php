fbf456787d503c550856c222d576b2e691f93578
mukolamusiv <mukolamusiv@gmail.com>
2022-07-01 22:09:05 +0300

    Відредагована міграція
    Додані поля до таблиці користувача

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('birthday')->nullable();
+            $table->string('address')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->timestamp('deleted_at');
         });
     }
 
