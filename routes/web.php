544b09699231d56db921ff4e7dc9d5200f987ccf
mukolamusiv <mukolamusiv@gmail.com>
2022-08-04 22:27:06 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', function () {
 //    return view('demo');
 //});
+
+
 Route::get('/{any}', function () {
     return view('test');})->where('any', '.*');
 //Route::get('/app/{vue_capture?}', function () {
