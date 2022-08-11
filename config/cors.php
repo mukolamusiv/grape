5507b310af75b6c359bca7aaa9d6fd233460c0a3
mukolamusiv <mukolamusiv@gmail.com>
2022-08-11 15:59:00 +0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -28,7 +28,7 @@ return [
 //
 //    'exposed_headers' => [],
 
-    'allowedOrigins' => [$origin, 'http://localhost:8000'],
+    'allowedOrigins' => [$origin, 'http://127.0.0.1:8000'],
     'allowedHeaders' => ['Content-Type', 'Accept', 'Authorization', 'X-Requested-With', 'Application'],
     'allowedMethods' => ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
     'exposedHeaders' => ['Authorization'],
