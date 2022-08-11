085f4d64b20bef5d450389a783133758402adc9a
mukolamusiv <mukolamusiv@gmail.com>
2022-08-11 16:49:31 +0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -18,20 +18,20 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-//    'allowed_methods' => ['*'],
-//
-//    'allowed_origins' => ['*'],
-//
-//    'allowed_origins_patterns' => [],
-//
-//    'allowed_headers' => ['*'],
-//
-//    'exposed_headers' => [],
+    'allowed_methods' => ['*'],
 
-    'allowedOrigins' => [$origin, 'http://127.0.0.1:8000'],
-    'allowedHeaders' => ['Content-Type', 'Accept', 'Authorization', 'X-Requested-With', 'Application'],
-    'allowedMethods' => ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
-    'exposedHeaders' => ['Authorization'],
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+//    'allowedOrigins' => [$origin, 'http://127.0.0.1:8000'],
+//    'allowedHeaders' => ['Content-Type', 'Accept', 'Authorization', 'X-Requested-With', 'Application'],
+//    'allowedMethods' => ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
+//    'exposedHeaders' => ['Authorization'],
 
     'max_age' => 0,
 
