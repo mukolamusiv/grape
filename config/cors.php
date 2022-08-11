dc38b163d619da5347f25797f0d999ea5d9a54a4
mukolamusiv <mukolamusiv@gmail.com>
2022-08-11 15:08:06 +0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -29,7 +29,8 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    //'supports_credentials' => false,
+    'supports_credentials' => true,
 //    header('Access-Control-Allow-Origin: *');
 //header('Access-Control-Allow-Methods: *');
 //header('Access-Control-Allow-Headers: *');
