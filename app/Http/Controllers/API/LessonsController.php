62792d64300503fd90d5e484d2e8f3c78ed568a8
mukolamusiv <mukolamusiv@gmail.com>
2022-08-20 08:26:22 +0300

    update vue + add water and lummen

--- app/Http/Controllers/API/LessonsController.php
+++ app/Http/Controllers/API/LessonsController.php
@@ -8,8 +8,11 @@ use App\Models\Topic;
 use App\Models\User;
 use App\Models\UserLessons;
 use App\Models\UserTopic;
+use Carbon\Carbon;
+use Faker\Core\DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class LessonsController extends Controller
 {
@@ -24,8 +27,38 @@ class LessonsController extends Controller
         foreach ($topics as $topic){
             $array[] = $topic->topic_id;
         }
-        $topics = Topic::whereNotIn('id',$array)->get();
-        return response($topics);
+        $topics = Topic::with('lessons')->whereNotIn('id',$array)->get();
+
+        $user_done = count(User::find(1)->topic_done);
+        $data = collect();
+        foreach ($topics as $topic){
+            if(empty($user_done)){
+                $water = 200;
+                $lumen = 150;
+            }else{
+                $lumen = 40*(count($topic->lessons)/$user_done);
+                $water = 60*(count($topic->lessons)/$user_done);
+            }
+            $topic = collect($topic);
+            $topic->put('water',round($water));
+            $topic->put('lumen',round($lumen));
+
+            //$topic->put('prise',100);
+           $data->push($topic);
+        }
+        //$to = Carbon::createFromFormat('Y-m-d H:s:i', date('Y-m-d H:s:i'));
+        //$from = Carbon::createFromFormat('Y-m-d H:s:i', User::find(1)->updated_at);
+
+        //$diff_in_minutes = $to->diffInMinutes($from);
+        //print_r($diff_in_minutes); // Output: 20
+        //return response($diff_in_minutes);
+//        $db = DB::table('personal_access_tokens')->where('tokenable_id','=','1')->orderByDesc('updated_at')->limit(1)->get();
+//
+//        $minutes = Carbon::createFromFormat('Y-m-d H:s:i', date('Y-m-d H:s:i'))->diffInMinutes(Carbon::createFromFormat('Y-m-d H:s:i', $db->first()->updated_at));
+//        $user = User::find(1);
+//        $user->energy = $user->energy + ($minutes/10);
+//        $user->save();
+        return response($data);
     }
 
     public function topics_active(){
