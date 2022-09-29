25113bad3a8adc0c31fe08f74f4c4e951fbdf61c
mukolamusiv <mukolamusiv@gmail.com>
2022-09-29 21:36:55 +0300

    spa up

---
+++ app/DTO/TestsDTO/FindPairDTO.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\DTO\TestsDTO;
+
+
+class FindPairDTO
+{
+    /**
+     * @var int
+     */
+    public int $lesson_id;
+    /**
+     * @var bool
+     */
+    public bool $completed = false;
+    /**
+     * @var object
+     */
+    public object $data;
+
+
+    
+}
