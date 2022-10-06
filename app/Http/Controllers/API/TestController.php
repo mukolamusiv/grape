6e183b3400d67e64a9419a613114d05ce18b2e21
mukolamusiv <mukolamusiv@gmail.com>
2022-10-06 22:51:10 +0300

    Update TestController.php

--- app/Http/Controllers/API/TestController.php
+++ app/Http/Controllers/API/TestController.php
@@ -138,6 +138,11 @@ class TestController extends Controller
 
     public function coloring_page(Request $request, $lesson_id){
         $data = new ColoringPageAnswer();
-        //$data->
+        $data->user_id = 1;
+        $data->lesson_id = $lesson_id;
+        $data->title = 'Розмальовка користувача';
+        $data->svg = $request->input('svg');
+        $data->save();
+        return response($data);
     }
 }
