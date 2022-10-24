90613a78d26dd0d48843c938b49e453a0a92cced
mukolamusiv <mukolamusiv@gmail.com>
2022-10-24 19:13:24 +0300

    Update TopicController.php

--- app/Http/Controllers/API/TopicController.php
+++ app/Http/Controllers/API/TopicController.php
@@ -2,84 +2,48 @@
 
 namespace App\Http\Controllers\API;
 
+use App\DTO\TopicDTO;
+use App\DTO\TopicsDTO;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TopicController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function done(){
+        $data = new TopicsDTO(Auth::id());
+        return response($data->getDone());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function active(){
+        $data = new TopicsDTO(Auth::id());
+        return response($data->getActive());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function all(){
+        $data = new TopicsDTO(Auth::id());
+        return response($data->getTopics());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+
+    public function user_done($user_id){
+        $data = new TopicsDTO($user_id);
+        return response($data->getDone());
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+    public function user_active($user_id){
+        $data = new TopicsDTO($user_id);
+        return response($data->getActive());
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+    public function user_all($user_id){
+        $data = new TopicsDTO($user_id);
+        return response($data->getTopics());
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+    public function getTopic($topic_id){
+        $data = new TopicDTO($topic_id);
+        return response($data->getTopic());
     }
+
 }
