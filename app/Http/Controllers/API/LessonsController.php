5fb382425f8f33b69e3b64d74e7d52c6bcedbc99
mukolamusiv <mukolamusiv@gmail.com>
2022-10-24 19:48:35 +0300

    fix

--- app/Http/Controllers/API/LessonsController.php
+++ app/Http/Controllers/API/LessonsController.php
@@ -332,21 +332,23 @@ class LessonsController extends Controller
     }
 
     public function topic($id){
+        $data = new TopicDTO($id,Auth::id());
+        return response($data->getTopic());
         $request = collect(UserTopic::with('topic')->where(['topic_id'=>$id,'user_id'=> Auth::id()])->get());
-        if($request->isNotEmpty()){
-            //$data = collect(UserTopic::with('topic')->find($request->first()->id));
-            //$data->forget('id');
-            //$data = collect();
-           // $data->push($request->first()->topic);
-            //$data->put('status',50);
-//            $data->put('complete',$request->first()->);
-            //$data->put('topic_id',$data->id);
-            return response($this->data_topic(UserTopic::with('topic')->find($request->first()->id)));
-        }else{
-            $data = $this->data_topic_available(Topic::with('lessons')->findOrFail($id));
-            $data->forget('status');
-            return response($data);
-        }
+//        if($request->isNotEmpty()){
+//            //$data = collect(UserTopic::with('topic')->find($request->first()->id));
+//            //$data->forget('id');
+//            //$data = collect();
+//           // $data->push($request->first()->topic);
+//            //$data->put('status',50);
+////            $data->put('complete',$request->first()->);
+//            //$data->put('topic_id',$data->id);
+//            return response($this->data_topic(UserTopic::with('topic')->find($request->first()->id)));
+//        }else{
+//            $data = $this->data_topic_available(Topic::with('lessons')->findOrFail($id));
+//            $data->forget('status');
+//            return response($data);
+//        }
     }
 
 
