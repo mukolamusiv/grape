17b5dbe3ea0c5c8a2076ca63e8e397f8e7e0250d
mukolamusiv <mukolamusiv@gmail.com>
2023-11-30 14:59:32 +0200

    Update CrosswordDTO.php

--- app/DTO/TestsDTO/CrosswordDTO.php
+++ app/DTO/TestsDTO/CrosswordDTO.php
@@ -100,7 +100,7 @@ class CrosswordDTO
             $this->completed = true;
         }else{
             $this->completed = false;
-            dd($this->completed);
+            dd($data);
         }
     }
 
